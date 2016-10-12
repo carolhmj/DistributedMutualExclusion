64c78521003e5efe94a88f3c2c47e19b83566c9f
99heitor <99heitor@gmail.com>
2016-10-11 23:55:56 -0300

    Cliente implementado. Corrigido bug no servidor relacionado a adicionar um cliente com a fila estando vazia e tentando tirá-lo da fila ao liberar o recurso.

---
+++ src/client/Client.java
@@ -0,0 +1,69 @@
+package client;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.Random;
+
+import server.ServerRequestManager;
+
+public class Client implements ClientRequestManager {
+	Random randInt;
+	ServerRequestManager remoteServer;
+	ClientRequestManager remoteClient;
+	Registry registry;
+	
+	@Override
+	public void receiveResource() throws RemoteException {
+		System.out.println("Obtained resource");
+	    try {
+	    	Thread.sleep(randInt.nextInt(6000));
+	    } catch (InterruptedException e) {
+
+	    }
+	    System.out.println("Resource use completed!");
+	    remoteServer.freeResource();
+	}
+	
+	public Client(){
+		
+		
+		randInt = new Random();
+
+		try {
+			registry = LocateRegistry.getRegistry("127.0.1.1",3232);
+			remoteServer = (ServerRequestManager) registry.lookup("rmiServer");
+	        remoteClient = (ClientRequestManager)UnicastRemoteObject.exportObject(this, 0);
+
+			
+		} catch (RemoteException | NotBoundException e) {
+			System.out.println("Couldn't find server on registry table.");
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void run(){
+		while(true){
+			try {
+				System.out.println("Resource requested.");
+
+				remoteServer.requestResource(remoteClient);
+				Thread.sleep(10000);
+			} catch (RemoteException | InterruptedException e) {
+				e.printStackTrace();
+			}
+
+		}
+		
+	}
+	
+	static public void main(String[] args) {
+
+		Client client = new Client();
+		client.run();
+	}
+
+}
